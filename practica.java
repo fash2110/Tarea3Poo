7a81968d4d835577a6d348b4320bc153fa400cfb
fash2110 <66839819+fash2110@users.noreply.github.com>
2022-09-23 19:55:16 -0600

    Delete practica.java

--- practica.java
+++
@@ -1,9 +0,0 @@
-public class practica {
-    static Funcionario f1 = new Funcionario("Pedro", 50000.00);
-    static Estudiante e1 = new Estudiante("Juan", 2022111222);
-
-    public static void main(String[] args){
-        System.out.println("Funcionario:" + f1.toString()+"\n");
-        System.out.println("Estudiante:"+e1.toString());
-    }
-}
